7b9024c2fb916d14d12b9cb4ac090d582c30cb46
jayasimhamadhira <46185623+jayasimhamadhira@users.noreply.github.com>
2019-03-19 18:36:04 +0530

    Update Powershell.ps1

--- Powershell.ps1
+++ Powershell.ps1
@@ -1,7 +1,7 @@
-    [string]$ServerName="credscansql.database.windows.net"
+ [string]$ServerName="sqlcredscan.database.windows.net"
     [string]$UserName="jayasimha"
     [string]$Password="Admin@12345678"
-    $DbName="CredscanSQL"
+    $DbName="sqlcredscan"
     $Clonedir = "C:\Credscan\Repoclone"
     $RgName="Credscan-RG"  
     $RgLocation="Central US"
@@ -12,10 +12,10 @@
     {
         Install-Module -Name AzureRm -AllowClobber -Force -Verbose
     }
-   $EU="
-    $EP"
-    $Subscription=""
-    $AzureSubscriptionTenantId=""
+    $EU="svcenvdv@microsoft.com"
+    $EP="P@sw0rd!13"
+    $Subscription="c1bd9039-9169-41b6-9b75-6eef04aaf8a4"
+    $AzureSubscriptionTenantId="72f988bf-86f1-41af-91ab-2d7cd011db47"
     $azureAccountName = $EU
     $azurePassword = ConvertTo-SecureString $EP -AsPlainText -Force
     $psCred = New-Object System.Management.Automation.PSCredential($azureAccountName, $azurePassword)
@@ -134,5 +134,3 @@ do
     }
 }
 until ($count -eq 0)
-
-
