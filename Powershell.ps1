c74760e20c7881633f34f69cf3aacd527625c9cf
jayasimhamadhira <46185623+jayasimhamadhira@users.noreply.github.com>
2019-04-04 19:09:24 +0000

    Update Powershell.ps1

--- Powershell.ps1
+++ Powershell.ps1
@@ -1,21 +1,22 @@
- [string]$ServerName=""
-    [string]$UserName=""
-    [string]$Password=""
-    $DbName=""
-    $Clonedir = "C:\Credscan\Repoclone"
-    $RgName="Credscan-RG"  
+    [string]$ServerName="sqlcredscan.database.windows.net"
+    [string]$UserName="jayasimha"
+    [string]$Password="Admin@12345678"
+    $DbName="sqlcredscan"
+    $RgName="Credscan-TFSV-RG"  
     $RgLocation="Central US"
     $storageaccountName="credscanrepo"
     $ContainerName="credscan-container"
+    $tfvcRepositoryPath = "C:\TFVCRepopath"
+
     $Armexist=Get-Module -Name AzureRM.* -ListAvailable
     if(!($Armexist.Count -gt 0))
     {
         Install-Module -Name AzureRm -AllowClobber -Force -Verbose
     }
-    $EU=""
-    $EP=""
-    $Subscription=""
-    $AzureSubscriptionTenantId=""
+    $EU="svcenvdv@microsoft.com"
+    $EP="P@sw0rd!13"
+    $Subscription="c1bd9039-9169-41b6-9b75-6eef04aaf8a4"
+    $AzureSubscriptionTenantId="72f988bf-86f1-41af-91ab-2d7cd011db47"
     $azureAccountName = $EU
     $azurePassword = ConvertTo-SecureString $EP -AsPlainText -Force
     $psCred = New-Object System.Management.Automation.PSCredential($azureAccountName, $azurePassword)
@@ -47,90 +48,101 @@ $count=1
 $connectionString = "Server=$ServerName;uid=$UserName; pwd=$Password;Database=$DbName;Integrated Security=False;"
 $connection = new-object system.data.SqlClient.SQLConnection($connectionString)
 Write-output "credential formed"
-do
-{
-    try
-    {
-      $connection.Open()
-     $command2 = $connection.CreateCommand() 
-    $command2.CommandText = "EXEC dbo.usp_GetAvailableRepository" 
+
+ function CloneTFVCRepo($prjName,$url,$PAT,$tfvcRepositoryPath,$tfvctoolpath)
+    {         
+        try
+        {
+            $cs_userName = "v-jamad@microsoft.com"            
+
+            New-Item -ItemType Directory -Path "$tfvcRepositoryPath\$prjName" -Force
+            cd $tfvcRepositoryPath\$prjName                    
+              #& "$tfvctoolpath\TFVCCloningExe\TFVCCloning.exe" $url $cs_userName $PAT $prjName gdbldsvc@microsoft.com $/$prjName $tfvcRepositoryPath\$prjName 
+            & "$tfvctoolpath\HelperTool\TFVCCloning.exe" $url $cs_userName $PAT $prjName v-jamad@microsoft.com $/$prjName $tfvcRepositoryPath\$prjName             
+        }
+        catch [System.Exception]
+        {
+            $message=$_ -replace "The running command stopped because the preference variable ""ErrorActionPreference"" or common parameter is set to Stop: ",""    
+            throw "$message"+"~Cloning"
+     
+        }        
+    }  
+
+    #New-Item -ItemType directory -Path "C:\TFVCRepopathCSV"
+   # New-Item -ItemType directory -Path "C:\TFVCRepopath"
+
+    $count=1
+
+    do
+   {
+
+    $connection.Open()
+    $command2 = $connection.CreateCommand() 
+    $command2.CommandText = "Exec usp_GetTFVCRepository" 
     $dataAdapt = new-object System.Data.SqlClient.SqlDataAdapter $command2
     $dataS2 = New-Object System.Data.DataSet
    Write-Host $dataAdapt.Fill($dataS2)       
    Write-Host $dataS2.Tables.Count
-    #$readerresult=$command.ExecuteReader()
-    if($dataS2.Tables.Count -gt 0 -And $dataS2.Tables[0].Rows.Count -gt 0)
-    #if($readerresult.HasRows)
+   $connection.Close()
+
+   if($dataS2.Tables.Count -gt 0 -And $dataS2.Tables[0].Rows.Count -gt 0)
+    
     {    
-        $count = 1
-                #while ($readerresult.Read())
-                #{            
-                    
-                    $repopath =  $dataS2.Tables[0].Rows[0]["Repopath"]
-                    $RepoName = $dataS2.Tables[0].Rows[0]["RepoName"]
-                    $RepoId = $dataS2.Tables[0].Rows[0]["RepoID"]
-                   # break;
-                                  
-                #}
+   $count = 1
+                    $repopath1 =  $dataS2.Tables[0].Rows[0]["OrganizationURL"]
+                    $RepoName1 = $dataS2.Tables[0].Rows[0]["ProjectName"]
+                    $RepoId1 = $dataS2.Tables[0].Rows[0]["ProjID"]
+                
             }
             else
             {
             $count = 0
             }
-           # $readerresult.Close()
-            if ($count -eq 1)
-            {
 
-       Write-output "entered into while block"
-        #$query = “select top 1 * from SQLCredscan where IsAccessed = 0 and IsProcessed= 0” #Remove this code
-        #$command = new-object system.data.sqlclient.sqlcommand($query,$connection)
-        #$connection.Open()
-        #$adapter = New-Object System.Data.sqlclient.sqlDataAdapter $command
-        #$dataset = New-Object System.Data.DataSet
-        #$adapter.Fill($dataSet) | Out-Null
-        $connection.Close()
-        #$table=$dataSet.Tables
-        #$repopath=$table.Rows["Repopath"]
-        #$RepoName=$table.Rows["RepoName"]
-        $dir = "C:\Repoclone\$RepoName"  
-        $toolPath ="C:\Credscan\tools\CredentialScanner.exe" 
-        $searcher="C:\Credscan\tools\Searchers\buildsearchers.xml"
-        $repoLogsOutput="C:\CSV\$RepoName"
-        Write-Output "===Cloning repo $repopath===" 
-        git clone $repopath $dir
-        #$connection = New-Object System.Data.SqlClient.SqlConnection
-        #$connection.ConnectionString = $connectionString
-        #$connection.Open()
-        #$query = “update SQLCredscan set IsAccessed=1 where RepoName='$RepoName' ” #Remove this code
-        #$command = $connection.CreateCommand()
-       # $command.CommandText = $query
-       # $result = $command.ExecuteReader()
-        Write-Output "$toolPath $dir $repoLogsOutput"
-        & $toolPath -I "$dir" -S $searcher -O "$repoLogsOutput" -f csv -cp
-       # $connection = New-Object System.Data.SqlClient.SqlConnection
-       # $connection.ConnectionString = $connectionString
-        $connection.Open()
-        $query1 = "update Credscan12 set IsProcessed=1, IsAccessed = 0 where RepoID='$RepoId' "
+
+
+
+
+    CloneTFVCRepo -prjName $RepoName1 -url $repopath1 -PAT "levn24wy5wmx2gkyv5qo64smhu3yj4wkqyn572n3hmaxnxusjnjq" -tfvctoolpath "C:\TFVCCloneTool" -tfvcRepositoryPath "C:\TFVCRepopath"
+
+    ExecCredScan -projectName $RepoName1 -repoName $RepoName1 -searchersPath "C:\Credscan\tools\Searchers\buildsearchers.xml" -toolPath "C:\Credscan\tools\CredentialScanner.exe" -repositoryPath "C:\TFVCRepopath" -repoLogsOutput "C:\TFVCRepopathCSV"
+    
+
+     Set-AzureStorageBlobContent -Container $ContainerName -File "C:\TFVCRepopathCSV\$RepoName1-matches.csv" -Context $storeageaccount.Context -Force
+
+         $connection.Open()
+         $query1 = "update TFVC_CredScan set IsProcessed=1, IsAccessed = 0 where ProjID='$RepoId1' "
         $command1 = $connection.CreateCommand()
         $command1.CommandText = $query1
         $result = $command1.ExecuteReader()
          $connection.Close()
 
-        Set-AzureStorageBlobContent -Container $ContainerName -File "$repoLogsOutput-matches.csv" -Context $storeageaccount.Context -Force
-        Write-Output "====Scan Completed and status updated===="
-       # $connection = New-Object System.Data.SqlClient.SqlConnection
-        #$connection.ConnectionString = $connectionString
-        #$connection.Open()
-        #$query = “select count (isprocessed) from SQLCredscan where isprocessed=0” #remove this code
-        #$command = $connection.CreateCommand()
-        #$command.CommandText = $query
-        #$count = $command.ExecuteScalar()  #remove this code
-        #$connection.Close()
+    }
+    until ($count -eq 0)
+    
+    function ExecCredScan($projectName, $repoName, $searchersPath, $toolPath, $repoType, $accountName,$repositoryPath,$repoLogsOutput)
+    {     
+        try
+        {
+
+        #$repoLogsOutput = "$resultsPath\$accountName!!$projectName!!$repoName!!$repoType$Using:InstanceNo"     
+            $dir = "$repositoryPath\$repoName"       
+
+            $repoLogsOutput="C:\TFVCRepopathCSV\$projectName"
+            Write-Output "$repoLogsOutput"
+      
+            & $toolPath -I "$dir" -S "$searchersPath" -O "$repoLogsOutput" -f csv -cp
         }
+        catch [System.Exception]
+        {
+            $message=$_ -replace "The running command stopped because the preference variable ""ErrorActionPreference"" or common parameter is set to Stop: ",""    
+            throw "$message"+"~Scanning"
+        }  
     }
-    catch
-    {
-    throw $_.Exception
-    }
-}
-until ($count -eq 0)
+
+   
+      # Set-AzureStorageBlobContent -Container $ContainerName -File "C:\TFVCRepopathCSV\$RepoName1-matches.csv" -Context $storeageaccount.Context -Force
+        Write-Output "====Scan Completed and status updated===="
+   
+
+#until ($count -eq 0)
